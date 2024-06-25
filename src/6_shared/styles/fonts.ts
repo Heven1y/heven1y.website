776ff8909e51863d43fe7a98a712cc20d0e0d30f
Heven1y <komrad.bizin@yandex.ru>
2024-06-25 18:41:08 +0600

    fix(shared): Fix build

--- src/6_shared/styles/fonts.ts
+++ src/6_shared/styles/fonts.ts
@@ -3,39 +3,39 @@ import localFont from "next/font/local";
 export const poppins = localFont({
   src: [
     {
-      path: "../../public/font/Poppins-Black.ttf",
+      path: "../../../public/font/Poppins-Black.ttf",
       weight: "900",
     },
     {
-      path: "../../public/font/Poppins-ExtraBold.ttf",
+      path: "../../../public/font/Poppins-ExtraBold.ttf",
       weight: "800",
     },
     {
-      path: "../../public/font/Poppins-Bold.ttf",
+      path: "../../../public/font/Poppins-Bold.ttf",
       weight: "700",
     },
     {
-      path: "../../public/font/Poppins-SemiBold.ttf",
+      path: "../../../public/font/Poppins-SemiBold.ttf",
       weight: "600",
     },
     {
-      path: "../../public/font/Poppins-Medium.ttf",
+      path: "../../../public/font/Poppins-Medium.ttf",
       weight: "500",
     },
     {
-      path: "../../public/font/Poppins-Regular.ttf",
+      path: "../../../public/font/Poppins-Regular.ttf",
       weight: "400",
     },
     {
-      path: "../../public/font/Poppins-Light.ttf",
+      path: "../../../public/font/Poppins-Light.ttf",
       weight: "300",
     },
     {
-      path: "../../public/font/Poppins-ExtraLight.ttf",
+      path: "../../../public/font/Poppins-ExtraLight.ttf",
       weight: "200",
     },
     {
-      path: "../../public/font/Poppins-Thin.ttf",
+      path: "../../../public/font/Poppins-Thin.ttf",
       weight: "100",
     },
   ],
