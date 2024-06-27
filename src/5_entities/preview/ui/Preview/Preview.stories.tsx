0299489ee05965d2ffb4e1c5037c468792ce6d7c
Heven1y <komrad.bizin@yandex.ru>
2024-06-27 22:51:57 +0600

    feat(entities): Add new entitie - Preview

---
+++ src/5_entities/preview/ui/Preview/Preview.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Meta, StoryFn } from "@storybook/react";
+import Preview from "./index";
+
+export default {
+  title: "Entities/Preview",
+  component: Preview,
+} as Meta;
+
+const Template: StoryFn<typeof Preview> = () => <Preview />;
+
+export const Primary = Template.bind({});
